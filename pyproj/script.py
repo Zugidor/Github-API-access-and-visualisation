a05deec3a9aa50a567c21688fca85e93a1863404
Zugidor <zugidor@tutanota.com>
2021-12-18 22:21:25 +0000

    Update script.py

--- pyproj/script.py
+++ pyproj/script.py
@@ -10,17 +10,19 @@ def main():
     token = os.getenv("GITHUB_TOKEN", "no token")  # get token from environment variable
     g = Github(token)  # create github object
     usr = g.get_user()  # get user object
+    myloc = usr.location  # get user location
     # connect to mongo db
     client = pymongo.MongoClient("mongodb://localhost:27017/")
     db = client["github"]
-    # clear collections, if any, start empty
+    # clear collections, if any, and start empty
     db.followers.drop()
     db.following.drop()
     followers = db["followers"]
     following = db["following"]
+    # get user followers and following
     f1 = usr.get_followers()
     f2 = usr.get_following()
-    # add followers and followees to DB, while avoiding duplicates
+    # add followers and following to DB, while avoiding duplicates
     for f in f1:
         dct = makeDict(f.login, g)
         followers.update_one(dct, {"$set": dct}, upsert=True)
@@ -30,7 +32,9 @@ def main():
 
 
 def makeDict(username, g):
-    usr = g.get_user(username)  # get user object
+    # get user object
+    usr = g.get_user(username)
+    # create dictionary with relevant info
     dct = {
             "login": usr.login,
             "name": usr.name,
@@ -40,6 +44,7 @@ def makeDict(username, g):
     for k, v in dct.items():
         if v is None:
             del dct[k]
+    # return dictionary
     return dct
 
 
