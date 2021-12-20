1c28c54640ab4e8068237558f92d0e7b9a1e505a
Zugidor <zugidor@tutanota.com>
2021-12-20 12:20:27 +0000

    format locs before processing and remove locs in CSV

--- pyproj/script.py
+++ pyproj/script.py
@@ -9,8 +9,8 @@ import csv
 
 def main():
 
-    print("This tool inspects and returns CSV with the locations of all the followers and following users of a "
-          "given GitHub user.")  # print info
+    print("This tool inspects and returns CSV with the timezones "
+          "of all the followers and following users of a given GitHub user.")  # print info
     usrnm = input("Enter the Github login username of the account you wish to inspect: ")  # get username
     token = os.getenv("GITHUB_TOKEN", "no token")  # get token from environment variable
     g = Github(token)  # create main github object from token
@@ -21,36 +21,32 @@ def main():
     # get user followers and following
     f1 = usr.get_followers()
     f2 = usr.get_following()
-    # get naive locations list
+    # get locations list
     loclist = get_loc_list(f1, f2, g)
-    # count locations and remove duplicates
-    locdict = {}
-    for loc in loclist:
-        if loc in locdict:
-            locdict[loc] += 1
-        else:
-            locdict[loc] = 1
 
     print("Writing to CSV...")
 
     # initialise CSV file
     csvfile = open("locations.csv", "w")
     csvwriter = csv.writer(csvfile)
-    csvwriter.writerow(["Location", "Region", "Timezone", "Count"])
+    csvwriter.writerow(["Region", "Timezone", "Count"])
 
-    # fill CSV file with data
+    # get regional and UTC timezones for each location, count occurrences, and write data to CSV
+    regdict = {}
     geoloc = Nominatim(user_agent="github_locations")  # initialise Nominatim API
-    for loc in locdict:
+    for loc in loclist:
         # get latitude and longitude
         gloc = geoloc.geocode(loc)
         if gloc is not None:
-            # get timezones
             reg = TimezoneFinder().timezone_at(lng=gloc.longitude, lat=gloc.latitude)  # get regional timezone
-            utc = timezone(reg).localize(datetime.now()).strftime("UTC%z")  # get UTC timezone
-            # write to CSV
-            csvwriter.writerow([loc, reg, utc, locdict[loc]])
-        else:
-            del[loc]  # remove location if it cannot be geocoded, as it is not a valid location
+            # build dictionary of regional timezones and their counts
+            if reg in regdict:
+                regdict[reg] += 1
+            else:
+                regdict[reg] = 1
+    for reg in regdict:
+        utc = timezone(reg).localize(datetime.now()).strftime("UTC%z")  # get UTC timezone
+        csvwriter.writerow([reg, utc, regdict[reg]])  # write to CSV
 
     print("CSV created,\nPython script done!")
 
@@ -62,7 +58,7 @@ def get_loc_list(followers, following, g):
     loclist = []
     for f in followers:
         loc = g.get_user(f.login).location
-        if loc is not None:
+        if loc is not None and loc != "" and not loc.__contains__("wa.me"):
             loclist.append(loc)
         count += 1
         if count == 300:
@@ -73,7 +69,7 @@ def get_loc_list(followers, following, g):
     count = 0
     for f in following:
         loc = g.get_user(f.login).location
-        if loc is not None:
+        if loc is not None and loc != "" and not loc.__contains__("wa.me"):
             loclist.append(loc)
         count += 1
         if count == 300:
@@ -84,6 +80,21 @@ def get_loc_list(followers, following, g):
     loc = g.get_user().location
     if loc is not None:
         loclist.append(loc)
+
+    # remove "/", "&", and "."
+    for loc in loclist:
+        if loc.__contains__("."):
+            newloc = loc.replace(".", "")
+            loclist.remove(loc)
+            loclist.append(newloc)
+        if loc.__contains__("/"):
+            newloc = loc[0:loc.find("/")]
+            loclist.remove(loc)
+            loclist.append(newloc)
+        if loc.__contains__("&"):
+            newloc = loc[0:loc.find("&")]
+            loclist.remove(loc)
+            loclist.append(newloc)
 
     return loclist
 
